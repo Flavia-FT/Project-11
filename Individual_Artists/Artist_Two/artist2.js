c83361bb2c04477c478280d6608c93397d53f260
pudders <113560351+pudders@users.noreply.github.com>
2022-11-02 23:50:16 +0000

    Update artist2.js

--- Individual_Artists/Artist_Two/artist2.js
+++ Individual_Artists/Artist_Two/artist2.js
@@ -2,10 +2,10 @@ let c1r1 = document.getElementById("col1_row1");
 
 
 c1r1.addEventListener("click", function(){
-  if(c1r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB1.jpg")
+  if(c1r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB1.jpg")
   {c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk1Tryagain.jpg"}
   else{
-    c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB1.jpg"
+    c1r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB1.jpg"
   }
 })
 
@@ -13,10 +13,10 @@ let c2r1 = document.getElementById("col2_row1");
 
 
 c2r1.addEventListener("click", function(){
-  if(c2r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB4.jpg")
+  if(c2r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB4.jpg")
   {c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb4ta.png"}
   else{
-    c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB4.jpg"
+    c2r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB4.jpg"
   }
 })
 
@@ -24,10 +24,10 @@ let c1r2 = document.getElementById("col1_row2");
 
 
 c1r2.addEventListener("click", function(){
-  if(c1r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB3.jpg")
+  if(c1r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB3.jpg")
   {c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb3ta.png"}
   else{
-    c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB3.jpg"
+    c1r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB3.jpg"
   }
 })
 
@@ -35,9 +35,9 @@ let c2r2 = document.getElementById("col2_row2");
 
 
 c2r2.addEventListener("click", function(){
-  if(c2r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB2.webp")
+  if(c2r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB2.webp")
   {c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb2tr.png"}
   else{
-    c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB2.webp"
+    c2r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB2.webp"
   }
 })
