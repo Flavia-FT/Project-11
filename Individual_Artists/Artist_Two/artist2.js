20443d0157417315bca11fa356bdeaeccdf28b75
pudders <113560351+pudders@users.noreply.github.com>
2022-11-02 23:32:36 +0000

    Add files via upload

---
+++ Individual_Artists/Artist_Two/artist2.js
@@ -0,0 +1,43 @@
+let c1r1 = document.getElementById("col1_row1");
+
+
+c1r1.addEventListener("click", function(){
+  if(c1r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB1.jpg")
+  {c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk1Tryagain.jpg"}
+  else{
+    c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB1.jpg"
+  }
+})
+
+let c2r1 = document.getElementById("col2_row1");
+
+
+c2r1.addEventListener("click", function(){
+  if(c2r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB4.jpg")
+  {c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb4ta.png"}
+  else{
+    c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB4.jpg"
+  }
+})
+
+let c1r2 = document.getElementById("col1_row2");
+
+
+c1r2.addEventListener("click", function(){
+  if(c1r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB3.jpg")
+  {c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb3ta.png"}
+  else{
+    c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB3.jpg"
+  }
+})
+
+let c2r2 = document.getElementById("col2_row2");
+
+
+c2r2.addEventListener("click", function(){
+  if(c2r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB2.webp")
+  {c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb2tr.png"}
+  else{
+    c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/PB2.webp"
+  }
+})
