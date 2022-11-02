8a841e33337da69cb343d847812a447cc51ab89b
pudders <113560351+pudders@users.noreply.github.com>
2022-11-02 23:51:03 +0000

    Update artist2.js

--- Individual_Artists/Artist_Two/artist2.js
+++ Individual_Artists/Artist_Two/artist2.js
@@ -3,7 +3,7 @@ let c1r1 = document.getElementById("col1_row1");
 
 c1r1.addEventListener("click", function(){
   if(c1r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB1.jpg")
-  {c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk1Tryagain.jpg"}
+  {c1r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/fk1Tryagain.jpg"}
   else{
     c1r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB1.jpg"
   }
@@ -14,7 +14,7 @@ let c2r1 = document.getElementById("col2_row1");
 
 c2r1.addEventListener("click", function(){
   if(c2r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB4.jpg")
-  {c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb4ta.png"}
+  {c2r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/pb4ta.png"}
   else{
     c2r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB4.jpg"
   }
@@ -25,7 +25,7 @@ let c1r2 = document.getElementById("col1_row2");
 
 c1r2.addEventListener("click", function(){
   if(c1r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB3.jpg")
-  {c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb3ta.png"}
+  {c1r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/pb3ta.png"}
   else{
     c1r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB3.jpg"
   }
@@ -36,7 +36,7 @@ let c2r2 = document.getElementById("col2_row2");
 
 c2r2.addEventListener("click", function(){
   if(c2r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB2.webp")
-  {c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/pb2tr.png"}
+  {c2r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/pb2tr.png"}
   else{
     c2r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_Two/PB2.webp"
   }
