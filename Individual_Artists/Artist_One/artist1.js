4326012327a511a0272065e7435ea98eae0688ff
pudders <113560351+pudders@users.noreply.github.com>
2022-11-02 22:34:59 +0000

    Update artist1.js

--- Individual_Artists/Artist_One/artist1.js
+++ Individual_Artists/Artist_One/artist1.js
@@ -2,10 +2,10 @@ let c1r1 = document.getElementById("col1_row1");
 
 
 c1r1.addEventListener("click", function(){
-  if(c1r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK1.jpg")
-  {c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk1Tryagain.jpg"}
+  if(c1r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK1.jpg")
+  {c1r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/fk1Tryagain.jpg"}
   else{
-    c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK1.jpg"
+    c1r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK1.jpg"
   }
 })
 
@@ -13,10 +13,10 @@ let c2r1 = document.getElementById("col2_row1");
 
 
 c2r1.addEventListener("click", function(){
-  if(c2r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK2.JPG")
-  {c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk2Tryagainsquare.jpg"}
+  if(c2r1.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK2.jpg")
+  {c2r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/fk2Tryagainsquare.jpg"}
   else{
-    c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK2.JPG"
+    c2r1.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK2.jpg"
   }
 })
 
@@ -24,10 +24,10 @@ let c1r2 = document.getElementById("col1_row2");
 
 
 c1r2.addEventListener("click", function(){
-  if(c1r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK4.webp")
+  if(c1r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK4.webp")
   {c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fkThat's%20correct.png"}
   else{
-    c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK4.webp"
+    c1r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK4.webp"
   }
 })
 
@@ -35,9 +35,9 @@ let c2r2 = document.getElementById("col2_row2");
 
 
 c2r2.addEventListener("click", function(){
-  if(c2r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK3.jpg")
-  {c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk2Tryagainsquare.jpg"}
+  if(c2r2.src == "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK3.jpg")
+  {c2r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/fk2Tryagainsquare.jpg"}
   else{
-    c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK3.jpg"
+    c2r2.src = "https://pudders.github.io/Course-Project-11-new/Individual_Artists/Artist_One/FK3.jpg"
   }
 })
