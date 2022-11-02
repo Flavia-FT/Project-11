5583a15c1b947c6bf544a4563aa82e717c87842a
pudders <113560351+pudders@users.noreply.github.com>
2022-11-02 22:29:05 +0000

    Update artist1.js

--- Individual_Artists/Artist_One/artist1.js
+++ Individual_Artists/Artist_One/artist1.js
@@ -2,10 +2,10 @@ let c1r1 = document.getElementById("col1_row1");
 
 
 c1r1.addEventListener("click", function(){
-  if(c1r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%20-%201%20Two%20nudes%20in%20a%20forest%201939.jpg")
+  if(c1r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK1.jpg")
   {c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk1Tryagain.jpg"}
   else{
-    c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%20-%201%20Two%20nudes%20in%20a%20forest%201939.jpg"
+    c1r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK1.jpg"
   }
 })
 
@@ -13,10 +13,10 @@ let c2r1 = document.getElementById("col2_row1");
 
 
 c2r1.addEventListener("click", function(){
-  if(c2r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%202%20-%20Sun%20and%20Life%201947.jpg")
+  if(c2r1.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK2.JPG")
   {c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk2Tryagainsquare.jpg"}
   else{
-    c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%202%20-%20Sun%20and%20Life%201947.jpg"
+    c2r1.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK2.JPG"
   }
 })
 
@@ -24,10 +24,10 @@ let c1r2 = document.getElementById("col1_row2");
 
 
 c1r2.addEventListener("click", function(){
-  if(c1r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%204%20-%20From%20our%20fake%20fruit%20garden%201969.webp")
+  if(c1r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK4.webp")
   {c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fkThat's%20correct.png"}
   else{
-    c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%204%20-%20From%20our%20fake%20fruit%20garden%201969.webp"
+    c1r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK4.webp"
   }
 })
 
@@ -35,9 +35,9 @@ let c2r2 = document.getElementById("col2_row2");
 
 
 c2r2.addEventListener("click", function(){
-  if(c2r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%203%20-%20Henry%20Ford%20hospital%201932.jpg")
+  if(c2r2.src == "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK3.jpg")
   {c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/fk2Tryagainsquare.jpg"}
   else{
-    c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK%203%20-%20Henry%20Ford%20hospital%201932.jpg"
+    c2r2.src = "file:///C:/Users/njpud/OneDrive/Desktop/CFG/Project/FK3.jpg"
   }
 })
